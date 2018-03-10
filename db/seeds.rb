dc845c238d845680fe2070783381e82969b8174b
jkahwati <jesuskahwati@hotmail.com>
2018-03-10 09:46:58 -0300

    Order folders

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(name: "jesus", last_name: "kahwati")
+User.create(name: "Gladys", last_name: "Aguilar")
+User.create(name: "Georges", last_name: "Beyloune")
